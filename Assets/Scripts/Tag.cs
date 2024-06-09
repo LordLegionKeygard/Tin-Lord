96d3cea5b663e7c4a1225db7cbead996fd2fe7b4
Dima Kalyuzhnui <dskeygard@mail.ru>
2024-06-09 23:03:54 +0300

    Sawmill, Tags

---
+++ Assets/Scripts/Tag.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tag : MonoBehaviour
+{
+    [SerializeField] private GameObject _parentObject;
+    [SerializeField] private Tags _currentTag;
+    public void CheckTag(Tags[] tags)
+    {
+        for (int i = 0; i < tags.Length; i++)
+        {
+            if (_currentTag == tags[i] || tags[0] == Tags.All)
+            {
+                Destroy(_parentObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,5 @@
+public enum Tags
+{
+    All = 0,
+    Tree = 1,
+}
