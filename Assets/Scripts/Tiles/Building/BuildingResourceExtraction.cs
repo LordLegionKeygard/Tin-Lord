7563e489c6918d4a092f1f1bb83fda3d1b6a875d
Dima Kalyuzhnui <dskeygard@mail.ru>
2024-07-03 00:41:50 +0300

    FixGetREsourcesModifier

--- Assets/Scripts/Tiles/Building/BuildingResourceExtraction.cs
+++ Assets/Scripts/Tiles/Building/BuildingResourceExtraction.cs
@@ -7,6 +7,6 @@ public class BuildingResourceExtraction : MonoBehaviour
     public void UpdateExtraction(Tile tile, int level, TileObject tileObject)
     {
         if (tile.Resource == null) return;
-        CustomEvents.FireChangeResourceExtraction(tile.Resource.ResourceEnum, tile.UpgradeBuildingWrapper[level - 1].RecourcesAmount * ResourceModifier(tileObject), tileObject.CurrentTileId());
+        CustomEvents.FireChangeResourceExtraction(tile.Resource.ResourceEnum, tile.UpgradeBuildingWrapper[level - 1].RecourcesAmount * StaticMethods.GetResourceModifier(tileObject), tileObject.CurrentTileId());
     }
 }
