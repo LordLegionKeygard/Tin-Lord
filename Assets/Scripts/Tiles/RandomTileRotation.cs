4b97d239df549fe07a264b12569fe3d4b4c3bc5c
Dima Kalyuzhnui <dskeygard@mail.ru>
2024-07-17 23:20:46 +0300

    WindPump, ReworkRotations

--- Assets/Scripts/Tiles/RandomTileRotation.cs
+++ Assets/Scripts/Tiles/RandomTileRotation.cs
@@ -3,42 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Случайным образом вращает тайл земли по оси y. Так же сетит значение в groundTile, чтобы здание могло считать текущую позицию.
+/// Случайным образом вращает объект по оси Y кратно 90.
 /// </summary>
 public class RandomTileRotation : MonoBehaviour
-{
-    [SerializeField] private int _parentCount;
-    [SerializeField] private bool _isSetGroundModelRotation;
-    private GroundTile _groundTile;
-    private void Start()
+{ 
+    private void Awake()
     {
-        if (_isSetGroundModelRotation) SetGroundTile();
         RandomRotation();
-    }
-    
-    private void SetGroundTile()
-    {
-        switch (_parentCount)
-        {
-            case 3:
-                _groundTile = gameObject.transform.parent.parent.parent.GetComponent<GroundTile>();
-
-                break;
-            case 4:
-                _groundTile = gameObject.transform.parent.parent.parent.parent.GetComponent<GroundTile>();
-
-                break;
-        }
     }
 
     private void RandomRotation()
     {
-        var rnd = Random.Range(0, 3);
+        var rnd = Random.Range(0, 4);
         var newRotation = rnd * 90;
 
         transform.rotation = Quaternion.Euler(0, newRotation, 0);
-
-        if (_isSetGroundModelRotation) _groundTile.SetGroundModelRotation(newRotation);
     }
-
 }
---
+++ Assets/SetTileRotation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Сетит значение в groundTile, чтобы здание могло считать текущую позицию.
+/// </summary>
+public class SetTileRotation : MonoBehaviour
+{
+    [SerializeField] private int _parentCount;
+    private GroundTile _groundTile;
+    private void Start()
+    {
+        SetGroundTile();
+    }
+
+    private void SetGroundTile()
+    {
+        switch (_parentCount)
+        {
+            case 3:
+                _groundTile = gameObject.transform.parent.parent.parent.GetComponent<GroundTile>();
+
+                break;
+            case 4:
+                _groundTile = gameObject.transform.parent.parent.parent.parent.GetComponent<GroundTile>();
+
+                break;
+        }
+
+        _groundTile.SetGroundModelRotation((int)transform.eulerAngles.y);
+    }
+}
