27ee8a931af002b5533729187b07395c614e2f22
Dima Kalyuzhnui <dskeygard@mail.ru>
2025-01-02 22:07:35 +0300

    CompleteCommandCenter

--- Assets/Scripts/UI/1_CommandCenter/CommandCenterChangeLanguage.cs
+++ Assets/Scripts/UI/1_CommandCenter/CommandCenterChangeLanguage.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 public class CommandCenterChangeLanguage : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _headerText;
+    [SerializeField] private TextMeshProUGUI[] _buildingTypesNameTexts;
+    [SerializeField] private Tile[] _buildingTypesTiles;
+
 
     private void Start()
     {
         _headerText.text = Language.TextStatic[32];
+
+        for (int i = 0; i < _buildingTypesNameTexts.Length; i++)
+        {
+            _buildingTypesNameTexts[i].text = _buildingTypesTiles[i].Name[Language.LanguageNumber];
+        }
     }
 }
