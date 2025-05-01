28f44202e5c6c2742ef4579dac20182ad9b6ec95
Dima Kalyuzhnui <dskeygard@mail.ru>
2025-05-01 12:14:48 +0300

    FixTweenNull

--- Assets/Scripts/Animator/Menu/MenuTurretsRandomRotation.cs
+++ Assets/Scripts/Animator/Menu/MenuTurretsRandomRotation.cs
@@ -7,6 +7,8 @@ public class MenuTurretsRandomRotation : MonoBehaviour
     [SerializeField] private int _firstAngle;
     [SerializeField] private int _secondAngle;
     [SerializeField] private int _rotationSpeed;
+
+    private Tween _tween;
     private void Start()
     {
         StartCoroutine(RotateLoop());
@@ -16,20 +18,22 @@ public class MenuTurretsRandomRotation : MonoBehaviour
     {
         while (true)
         {
-            // Ждать случайное время перед вращением
             float delayBefore = Random.Range(0f, 3f);
             yield return new WaitForSeconds(delayBefore);
 
-            // Вращение на угол от 30 до -15
-            transform.DOLocalRotate(new Vector3(0, Random.Range(_firstAngle, _secondAngle), 0), _rotationSpeed, RotateMode.Fast)
-                     .SetEase(Ease.Linear);
+            _tween = transform.DOLocalRotate(new Vector3(0, Random.Range(_firstAngle, _secondAngle), 0), _rotationSpeed, RotateMode.Fast).SetEase(Ease.Linear);
 
-            // Ждать время пока крутится (чтобы следующее вращение не мешало)
+            // Ждем пока крутится (чтобы следующее вращение не мешало)
             yield return new WaitForSeconds(_rotationSpeed);
 
-            // Ждать случайное время после вращения
+            // Ждем случайное время после вращения
             float delayAfter = Random.Range(1f, 3f);
             yield return new WaitForSeconds(delayAfter);
         }
+    }
+
+    private void OnDestroy()
+    {
+        _tween.Kill();
     }
 }
