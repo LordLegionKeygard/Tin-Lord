f08b178bb766ec155e40207b5b01cc3e3dbcf927
Dima Kalyuzhnui <dskeygard@mail.ru>
2025-06-09 21:00:20 +0300

    CompleteMapGenerator

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    [SerializeField] private RectTransform _contentTransform;
+
     [Header("Node Pools")]
     [SerializeField] private NodeDataPool _nodeDataPool;
 
     [Header("Generation Settings")]
-    public int totalLayers = 5;
+    private int _totalLayers = 4;
     private float _xOffset = 300f;
     private float _yOffset = 300f;
-    private float _yRandomSpread = 100f;
+    private float _yRandomSpread = 50f;
+    private float _leftOffset = 100f;
 
     [SerializeField] private List<NodeInstance> _generatedNodes = new();
     public List<NodeInstance> GetGeneratedNodes() => _generatedNodes;
@@ -22,87 +25,99 @@ public class MapGenerator : MonoBehaviour
         _generatedNodes.Clear();
         _layers.Clear();
 
-        // Стартовая миссия
+        // Стартовая миссия (обязательно)
         MissionNode startMission = GetRandomNode(_nodeDataPool.Missions);
-        NodeInstance startInstance = new NodeInstance
-        {
-            nodeData = startMission,
-            layer = 0,
-            position = GetNodePosition(0, 0)
-        };
+        NodeInstance startInstance = CreateNode(startMission, 0, 0);
         _generatedNodes.Add(startInstance);
-        _layers[0] = new List<NodeInstance> { startInstance };
+        AddToLayer(0, startInstance);
 
-        // Промежуточные
-        List<NodeType> nodeTypes = new List<NodeType>
+        // Подготовка пулов
+        var missionPool = new List<MissionNode>(_nodeDataPool.Missions);
+        missionPool.Remove(startMission);
+        var eventPool = new List<EventNode>(_nodeDataPool.Events);
+        var traderPool = new List<TraderNode>(_nodeDataPool.Traders);
+        Shuffle(missionPool);
+        Shuffle(eventPool);
+        Shuffle(traderPool);
+
+        // Сначала размещаем все миссии в ближние слои
+        for (int layer = 1; layer < _totalLayers - 1; layer++)
         {
-            NodeType.Mission,
-            NodeType.Event,
-            NodeType.Event,
-            NodeType.Trader,
-            NodeType.Trader
-        };
+            if (missionPool.Count > 0)
+            {
+                NodeData mission = missionPool[0];
+                missionPool.RemoveAt(0);
 
-        Shuffle(nodeTypes);
-        int currentLayer = 1;
+                NodeInstance instance = CreateNode(mission, layer, 0);
+                _generatedNodes.Add(instance);
+                AddToLayer(layer, instance);
+            }
+        }
 
-        while (nodeTypes.Count > 0 && currentLayer < totalLayers - 1)
+        // Гарантируем что все промежуточные слои существуют заранее
+        for (int layer = 1; layer < _totalLayers - 1; layer++)
+        {
+            if (!_layers.ContainsKey(layer))
+                _layers[layer] = new List<NodeInstance>();
+        }
+
+        // Теперь дополняем оставшиеся слоты ивентами и торговцами
+        for (int layer = 1; layer < _totalLayers - 1; layer++)
         {
             int nodesInLayer = Random.Range(1, 3);
-            List<NodeInstance> layerNodes = new List<NodeInstance>();
+            List<NodeInstance> layerNodes = _layers[layer];
 
-            for (int i = 0; i < nodesInLayer; i++)
+            for (int i = layerNodes.Count; i < nodesInLayer; i++)
             {
-                if (nodeTypes.Count == 0) break;
+                NodeData nodeData = null;
 
-                NodeType type = nodeTypes[0];
-                nodeTypes.RemoveAt(0);
-
-                NodeData nodeData = GetNodeDataByType(type);
-                Vector2 position = GetNodePosition(currentLayer, i);
-                NodeInstance instance = new NodeInstance
+                if (eventPool.Count > 0)
                 {
-                    nodeData = nodeData,
-                    layer = currentLayer,
-                    position = position
-                };
+                    nodeData = eventPool[0];
+                    eventPool.RemoveAt(0);
+                }
+                else if (traderPool.Count > 0)
+                {
+                    nodeData = traderPool[0];
+                    traderPool.RemoveAt(0);
+                }
 
+                if (nodeData == null)
+                    continue;
+
+                NodeInstance instance = CreateNode(nodeData, layer, i);
                 _generatedNodes.Add(instance);
                 layerNodes.Add(instance);
             }
-
-            _layers[currentLayer] = layerNodes;
-            currentLayer++;
         }
 
         // Финальный босс
-        NodeInstance bossInstance = new NodeInstance
-        {
-            nodeData = _nodeDataPool.Boss,
-            layer = totalLayers - 1,
-            position = GetNodePosition(totalLayers - 1, 0)
-        };
+        NodeInstance bossInstance = CreateNode(_nodeDataPool.Boss, _totalLayers - 1, 0);
         _generatedNodes.Add(bossInstance);
-        _layers[totalLayers - 1] = new List<NodeInstance> { bossInstance };
+        AddToLayer(_totalLayers - 1, bossInstance);
 
         GenerateConnections();
 
         Debug.Log($"Карта сгенерирована: {_generatedNodes.Count} нодов");
     }
 
-    private NodeData GetNodeDataByType(NodeType type)
+    private void AddToLayer(int layer, NodeInstance instance)
     {
-        switch (type)
+        if (!_layers.ContainsKey(layer))
+            _layers[layer] = new List<NodeInstance>();
+
+        _layers[layer].Add(instance);
+    }
+
+    private NodeInstance CreateNode(NodeData data, int layer, int indexInLayer)
+    {
+        return new NodeInstance
         {
-            case NodeType.Mission:
-                return GetRandomNode(_nodeDataPool.Missions);
-            case NodeType.Event:
-                return GetRandomNode(_nodeDataPool.Events);
-            case NodeType.Trader:
-                return GetRandomNode(_nodeDataPool.Traders);
-            default:
-                return null;
-        }
+            nodeData = data,
+            layer = layer,
+            position = GetNodePosition(layer, indexInLayer),
+            connectedNodes = new List<NodeInstance>()
+        };
     }
 
     private T GetRandomNode<T>(List<T> list) where T : NodeData
@@ -121,33 +136,38 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
-
     private Vector2 GetNodePosition(int layer, int indexInLayer)
     {
-        float x = layer * _xOffset;
-        float y = (indexInLayer - 0.5f) * _yOffset + Random.Range(-_yRandomSpread, _yRandomSpread);
+        float totalWidth = _contentTransform.rect.width;
+        float xOffset = layer * _xOffset;
+        float x = xOffset - totalWidth / 2f + _leftOffset;
+        float y = -((indexInLayer - 0.5f) * _yOffset + Random.Range(-_yRandomSpread, _yRandomSpread));
         return new Vector2(x, y);
     }
 
     private void GenerateConnections()
     {
-        for (int layer = 0; layer < totalLayers - 1; layer++)
-        {
-            if (!_layers.ContainsKey(layer) || !_layers.ContainsKey(layer + 1))
-                continue;
+        var sortedLayers = new List<int>(_layers.Keys);
+        sortedLayers.Sort();
 
-            List<NodeInstance> currentLayerNodes = _layers[layer];
-            List<NodeInstance> nextLayerNodes = _layers[layer + 1];
+        for (int i = 0; i < sortedLayers.Count - 1; i++)
+        {
+            int currentLayer = sortedLayers[i];
+            int nextLayer = sortedLayers[i + 1];
+
+            List<NodeInstance> currentLayerNodes = _layers[currentLayer];
+            List<NodeInstance> nextLayerNodes = _layers[nextLayer];
 
             foreach (var currentNode in currentLayerNodes)
             {
-                int connections = Random.Range(1, 3);
+                int connections = Mathf.Min(nextLayerNodes.Count, Random.Range(1, 3));
+
                 var targets = new List<NodeInstance>(nextLayerNodes);
                 Shuffle(targets);
 
-                for (int i = 0; i < Mathf.Min(connections, targets.Count); i++)
+                for (int j = 0; j < connections; j++)
                 {
-                    currentNode.connectedNodes.Add(targets[i]);
+                    currentNode.connectedNodes.Add(targets[j]);
                 }
             }
         }
