83c03359bbb49cf5f2fc2232d1438b4efb0e1472
Dima Kalyuzhnui <dskeygard@mail.ru>
2025-08-14 03:09:47 +0300

    ColliderBugFix

--- Assets/Scripts/AiLogic/Bosses/SkillColliderDestroyTimer.cs
+++ Assets/Scripts/AiLogic/Bosses/SkillColliderDestroyTimer.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 
 public class SkillColliderDestroyTimer : MonoBehaviour
 {
-    [SerializeField] private Collider _collider;
     [SerializeField] private bool _needActivate;
     [SerializeField] private bool _needDeactivate;
     [SerializeField] private bool _needDestroy;
     [SerializeField] private float _activateTime;
     [SerializeField] private float _deactivateTime;
     [SerializeField] private float _destroyTime;
+    private Collider _collider;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+    }
 
 
     private void Start()
     {
-        if(_needActivate) StartCoroutine(ColliderActivate());
-        if(_needDeactivate) StartCoroutine(ColliderDeactivate());
-        if(_needDestroy) Destroy(gameObject, _destroyTime);
+        if (_needActivate) StartCoroutine(ColliderActivate());
+        if (_needDeactivate) StartCoroutine(ColliderDeactivate());
+        if (_needDestroy) Destroy(gameObject, _destroyTime);
     }
 
     private IEnumerator ColliderActivate()
