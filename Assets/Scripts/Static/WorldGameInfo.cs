a14b0d28ff4299b9e42b78199e3f2ea1b7ca1c75
Dima Kalyuzhnui <dskeygard@mail.ru>
2025-11-17 19:14:26 +0300

    RebalanceSpawners, Balance 3 spawner,

--- Assets/Scripts/Static/WorldGameInfo.cs
+++ Assets/Scripts/Static/WorldGameInfo.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class WorldGameInfo
 {
     [Header("Build")]
-    public static bool IsSteam = true;
-    public static bool IsDemo = true;
+    public static bool IsSteam = false;
+    public static bool IsDemo = false;
 
     [Header("Cards")]
     public static int ChanceAddOneCard = 50;
