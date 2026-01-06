e559d9266683a2a9cc585ae57982931196bc03ca
Dima Kalyuzhnui <dskeygard@mail.ru>
2026-01-06 20:18:25 +0300

    ChangeDemoDataSaveFileName

--- Assets/Scripts/Data/SpaceSaveGameDataWriter.cs
+++ Assets/Scripts/Data/SpaceSaveGameDataWriter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpaceSaveGameDataWriter
 {
     public string SaveDataDirectoryPath = "";
-    private string _dataSaveFileName => WorldGameInfo.IsDemo ? "IsDemoSpaceSave.txt" : "SpaceSave.txt";
+    private string _dataSaveFileName => WorldGameInfo.IsDemo ? "DemoSpaceSave.txt" : "SpaceSave.txt";
 
     public SpaceSaveGameDataWriter(string saveDataDirectoryPath)
     {
